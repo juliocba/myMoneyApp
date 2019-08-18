e5a5361a6d00e4f07dab40a17153f8680b9623c9
Júlio César Baltazar <juliocesar.jcba@gmail.com>
2019-08-18 17:22:13 -0300

    comeco da tela

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,11 +1,11 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.jsx',
     output: {
         path: __dirname + '/public',
-        filename: './app.js',
+        filename: './app.js'
     },
     devServer: {
         port: 8080,
@@ -14,33 +14,33 @@ module.exports = {
     resolve: {
         extensions: ['', '.js', '.jsx'],
         alias: {
-            modules: __direname + '/node_modules',
-            jquery: 'modules/admin-lte/plugins/jquery/jquery-2.2.3.min.js',
-            bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js',
+            modules: __dirname + '/node_modules',
+            jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
+            bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
         }
     },
-    plugins: [
+    plugins: [ 
         new webpack.ProvidePlugin({
             $: 'jquery',
-            jquery: 'jquery',
-            'window.jQuery': 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
         }),
         new ExtractTextPlugin('app.css')
     ],
     module: {
         loaders: [{
-            test: /.js[X]?/,
+            test: /.js[x]?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
                 presets: ['es2015', 'react'],
-                plugin: ['transform-object-rest-sread']
+                plugins: ['transform-object-rest-spread']
             }
         }, {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         }, {
-            test: /\.woff|.woff2|.tff|.eot|.svg|.png|.jpg*.*$/,
+            test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
             loader: 'file'
         }]
     }
